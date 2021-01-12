7eb00effc98b6b1079478a11c38be26b8e697726
Caitlyn <cgreffly@gmail.com>
2021-01-11 18:29:01 -0800

    I employed the use of React hooks useReducer and useEffect to load the images on demand, so that the thumbnail image would be the first to load and then the fullsize image would take its place once it was loaded. I added a keyframes animation to make the transition a bit smoother

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+import * as React from 'react';
 import './App.css';
-import Image from './Image';
+
+function imageProgression(currentImage, image) {
+  if (image.type === 'fullsize image') {
+    return image.src;
+  }
+  if (!currentImage) {
+    return image.src;
+  }
+  return currentImage;
+}
+
+function useProgressiveImage({ fullsize, thumbnail }) {
+  const [currentImage, dispatch] = React.useReducer(imageProgression);
+  React.useEffect(() => {
+    const fullsizeImage = new Image();
+    const thumbnailImage = new Image();
+
+    fullsizeImage.onload = () => {
+      dispatch({ type: 'fullsize image', src: fullsize });
+    };
+    thumbnailImage.onload = () => {
+      dispatch({ type: 'thumbnail image', src: thumbnail });
+    };
+
+    fullsizeImage.src = fullsize;
+    thumbnailImage.src = thumbnail;
+  }, [fullsize, thumbnail]);
+
+  return currentImage;
+}
 
 function App() {
+  const src = useProgressiveImage({
+    fullsize: 'images/NikeFullsize.jpg',
+    thumbnail: 'images/NikeThumb.jpg',
+  });
   return (
     <div className='App'>
       <header className='App-header'>Progressive Image Loading</header>
-      <Image />
+      <img
+        className='fullImage'
+        src={src}
+        alt='Sweet Nike sneaks in the street'
+      />
     </div>
   );
 }
